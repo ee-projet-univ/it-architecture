aea8dfce7dd96a68859f09224f9810304b335683
Emilien Escalle <emilien.escalle@escemi.com>
2021-02-03 10:27:40 +0100

    chore: reorganize by TDs

--- soap-client/index.js
+++
@@ -1,9 +0,0 @@
-var soap = require('soap');
-var url = 'http://localhost:8000/wsdl?wsdl';
-var args = {name: 'emilien'};
-
-soap.createClient(url, function(err, client) {
-    client.HelloWorld(args, function(err, result) {
-        console.log(result);
-    });
-});
